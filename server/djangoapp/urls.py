bb40f4af134ac5a0bc8531d99d3560307efe598f
lotfy2125 <saylotfy@gmail.com>
2022-05-26 18:19:33 +0000

    commit from the lab

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,18 +8,18 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-
     # path for about view
-
     # path for contact us view
-
     # path for registration
-
     # path for login
-
     # path for logout
 
     path(route='', view=views.get_dealerships, name='index'),
+    path('login/', view=views.login_request, name='login'),
+    path('registration/', view=views.registration_request, name='registration'),
+    path('about/', view=views.about_us, name='about'),
+    path('contact/', view=views.contact_us, name='contact'),
+    path('logout/', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+
+
+from .models import *
+from .restapis import *
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -16,33 +18,100 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 
-
 # Create an `about` view to render a static about page
 # def about(request):
 # ...
+def about_us(request):
+    
+        return render(request, 'djangoapp/about.html')
+
+
+
+
 
 
 # Create a `contact` view to return a static contact page
 #def contact(request):
+def contact_us(request):
+    
+        return render(request, 'djangoapp/contact.html')
+
+
 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
 # ...
 
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            context['message'] = "Invalid username or password."
+            return render(request, 'djangoapp/user_login_bootstrap.html', context)
+    else:
+        return render(request, 'djangoapp/user_login_bootstrap.html', context)
+
+
+
+
+
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
 # ...
+
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
 # ...
 
-# Update the `get_dealerships` view to render the index page with a list of dealerships
-def get_dealerships(request):
+def registration_request(request):
     context = {}
-    if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', context)
+    elif request.method == 'POST':
+        # Check if user exists
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.error("New user")
+        if not user_exist:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+            login(request, user)
+            return redirect("djangoapp:index")
+        else:
+            context['message'] = "User already exists."
+            return render(request, 'djangoapp/registration.html', context)
 
+
+
+
+# Update the `get_dealerships` view to render the index page with a list of dealerships
+
+def get_dealerships(request):
+    if request.method == "GET":
+        url = "https://86cac1fe.us-south.apigw.appdomain.cloud/delerships/entries"
+        # Get dealers from the URL
+        #dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        #dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return render( request , 'djangoapp/index.html')
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
